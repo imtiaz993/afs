4d0df73833db222b787ef5551ca86cc2323220b0
Imtiaz Hussain <imtiazh993@gmail.com>
2024-05-01 15:21:18 +0500

    'fixes'

--- app/(pages)/(home)/components/HomeHero.jsx
+++ app/(pages)/(home)/components/HomeHero.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const HomeHero = () => {
   return (
-    <div className="px-[4.17%] md:w-11/12 mx-auto py-10 lg:py-32 md:px-10 lg:px-16 flex relative home-hero-gradient rounded">
+    <div className="px-[4.17%] md:w-11/12 mx-auto py-10 lg:py-32 md:px-10 lg:px-16 flex relative home-hero-gradient rounded overflow-hidden">
       <div className="flex flex-col items-center md:items-start relative z-10">
         <h1 className="text-white text-center md:text-left  text-3xl md:text-5xl lg:text-[88px] leading-[100%] md:max-w-[500px] lg:max-w-[864px]">
           Secure, smart and simplified financial solutions
