bf92bba97f800832378391fa03356cb51c543086
Imtiaz Hussain <imtiazh993@gmail.com>
2024-06-14 23:07:32 +0500

    fixed 404 page

--- app/[locale]/not-found.jsx
+++ app/[locale]/not-found.jsx
@@ -1,6 +1,6 @@
 "use client";
-import Navbar from "./common/Navbar";
-import CommonHero from "./common/CommonHero";
+import Navbar from "../common/Navbar";
+import CommonHero from "../common/CommonHero";
 
 export default function NotFound() {
   return (
